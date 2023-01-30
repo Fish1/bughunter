73d12da4b98680e6522544c3d13210776aa5403c
Jacob Enders <>
2023-01-30 18:36:57 -0500

    test

---
+++ src/MyList.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+function MyList() {
+  [null as ReactNode].map(column => {
+    column?.toLocaleString;
+    const aa = column;
+  });
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import App from './App'
+
+async function myUnusedFunction() {
+  const fetch1 = Promise.resolve(['hello', 'world']);
+  const [data1] = await Promise.all([fetch1]);
+  const y = data1.map((data) => {
+    return data.toString();
+  });
+  return { y };
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      hello world
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
